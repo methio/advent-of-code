81d834ed0af5bbe381e6d7bc5e70a76dabbf0641
Thomas Perivolas <thomas.perivolas@gmail.com>
2025-12-02 09:17:29 +0100

    day1 - part 2

---
+++ 2025/1/2.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+fs.readFile('input.txt', 'utf8', (err, data) => {
+    if(err) throw err;
+
+    let currentPos = 50;
+    let azero = 0;
+
+    data.split('\n').forEach((l, index) => {
+        const dir = l.substring(0,1);
+        const val = parseInt(l.substring(1));
+
+        for(let i = 0; i < val ; i++){
+            if(dir == "L"){                  
+                currentPos--;                
+                if(currentPos < 0)currentPos = 99;
+                if(currentPos == 0)azero++;
+
+            }else if(dir == "R"){
+                    currentPos++;                
+                    if(currentPos > 99)currentPos = 0;  
+                    if(currentPos == 0)azero++;
+
+            }else{
+                console.log(`neither R or L at index ${index}`);
+            }
+        }
+    });
+    console.log(azero);
+});
